c4b135816325d7285bc48be78c9275c4bec15d1a
watcharabest <watcharabest.86@gmail.com>
2025-09-15 14:13:26 +0700

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const allowedDevOrigins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://10.100.109.48:3000",
+    "*"
 ];
 
 const nextConfig: NextConfig = {
